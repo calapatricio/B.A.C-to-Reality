c40ca4ab6beddb242498f89e2f230ace52a0ba1b
Victor <v.barbado@alumnos.upm.es>
2025-06-20 02:28:03 +0200

    Crear bebida layout y minor fixes

--- app/src/main/java/com/cala/bactoreality/AppDatabase.kt
+++ app/src/main/java/com/cala/bactoreality/AppDatabase.kt
@@ -1,9 +1,11 @@
 package com.cala.bactoreality
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [Bebida::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun bebidaDao() : BebidaDao
 
